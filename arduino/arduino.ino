cea25eabf31cbdef2ca2919d100ed2744afedb4c
Ahm3dGI1 <ahmedgouda797@gmail.com>
2023-01-13 18:29:48 +0200

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -85,7 +85,7 @@ void loop()
           counter++;
 
           //IR Code
-          float volts = analogRead(sensor)*0.0048828125;  // value from sensor * (5/1024)
+          float volts = analogRead(IRPIN)*0.0048828125;  // value from sensor * (5/1024)
           int distance = 13*pow(volts, -1); // worked out from datasheet graph
           delay(1000); // slow down serial port 
   
